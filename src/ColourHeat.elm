a6d463348b136fe342bc015160f0113a742feab1
Lachie Cox <lachiec@gmail.com>
2020-06-13 11:09:06 +1000

    improve colours

--- src/ColourHeat.elm
+++ src/ColourHeat.elm
@@ -23,27 +23,62 @@ total =
 
 
 type alias Stop =
-    { lo : Float
-    , hi : Float
-    , h : Float
-
-    -- , s : Float
-    -- , l : Float
+    { range : ( Float, Float )
+    , fader : ( Float, Float ) -> Float -> HSL
     }
 
 
+type alias HSL =
+    { h : Float, s : Float, l : Float }
+
+
+fadeDown : Float -> ( Float, Float ) -> Float -> HSL
+fadeDown baseHue ( lo, hi ) value =
+    let
+        prop =
+            proportion lo hi (value * 10)
+
+        h =
+            baseHue / total
+
+        s =
+            (1 - 0.5) * prop + 0.5
+
+        l =
+            (0.65 - 0.5) * (1 - prop) + 0.5
+    in
+    { h = h, s = s, l = l }
+
+
+fadeUp : Float -> ( Float, Float ) -> Float -> HSL
+fadeUp baseHue ( lo, hi ) value =
+    let
+        prop =
+            1 - proportion lo hi (value * 10)
+
+        h =
+            baseHue / total
+
+        s =
+            (1 - 0.5) * prop + 0.5
+
+        l =
+            (0.65 - 0.5) * (1 - prop) + 0.5
+    in
+    { h = h, s = s, l = l }
+
+
 bottomStop =
-    Stop 0 3 0
+    Stop ( 0, 5 ) (fadeUp 0)
 
 
 ranges =
     [ bottomStop
-    , Stop 3 5 5
-    , Stop 5 6 19
-    , Stop 6 7 43
-    , Stop 7 8 60
-    , Stop 8 9 200
-    , Stop 9 10 120
+    , Stop ( 5, 6 ) (fadeDown 290)
+    , Stop ( 6, 7 ) (fadeDown 70)
+    , Stop ( 7, 8 ) (fadeDown 170)
+    , Stop ( 8, 9 ) (fadeDown 200)
+    , Stop ( 9, 10 ) (fadeDown 120)
     ]
 
 
@@ -56,7 +91,11 @@ stop_ : List Stop -> Float -> Stop
 stop_ stops v =
     case stops of
         x :: rest ->
-            if x.lo <= v then
+            let
+                ( lo, _ ) =
+                    x.range
+            in
+            if lo <= v then
                 x
 
             else
@@ -92,22 +131,13 @@ heatMaybe lo hi maybeValue =
 heat : Float -> Float -> Float -> Element.Color
 heat _ _ value =
     let
-        { h, lo, hi } =
+        stop =
             findStop value
 
-        prop =
-            proportion lo hi (value * 10)
-
-        hRatio =
-            h / total
-
-        s =
-            (1 - 0.5) * prop + 0.5
-
-        l =
-            (0.65 - 0.5) * (1 - prop) + 0.5
+        { h, s, l } =
+            stop.fader stop.range value
     in
-    Color.hsl hRatio s l |> colorToElmUIColor
+    Color.hsl h s l |> colorToElmUIColor
 
 
 
